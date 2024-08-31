99260a34c4f75abb5a5563914eef5a8daceed3cc
Patryk Podworski <patryk@xtramile.no>
2024-08-31 12:10:22 +0200

    Update air table athlete record

---
+++ src/app/lib/athletes/updateAthlete.ts
@@ -0,0 +1,19 @@
+import config from "@/app/utils/config";
+import Athlete from "./Athlete";
+
+export const updateAthlete = async (id: string, data: InputData) => {
+  const url = `https://api.airtable.com/v0/${config.AIRTABLE_BASE_ID}/${config.AIRTABLE_TABLE}/${id}`;
+  const response = await fetch(url, {
+    method: "PATCH",
+    headers: {
+      Authorization: `Bearer ${config.AIRTABLE_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ fields: data }),
+  });
+
+  const updated = await response.json();
+  console.log(updated);
+};
+
+type InputData = Partial<Omit<Athlete, "id">>;
