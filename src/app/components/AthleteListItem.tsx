d9f870886114b69e1108a29ca4f960b9a17014af
Patryk Podworski <patryk@xtramile.no>
2024-09-01 08:49:55 +0200

    Add link to athlete

---
+++ src/app/components/AthleteListItem.tsx
@@ -0,0 +1,58 @@
+import clsx from "clsx";
+import Athlete from "@/app/lib/athletes/Athlete";
+import formatToHours from "@/app/utils/formatToHours";
+import formatToKilometers from "@/app/utils/formatToKilometers";
+
+export const AthleteListItem = ({
+  athlete,
+  statistics,
+  place,
+}: AthleteListItemProps) => {
+  return (
+    <a
+      key={athlete.userId}
+      className="flex gap-3 items-center p-2 cursor-pointer"
+      href={`https://www.strava.com/athletes/${athlete.userId}`}
+    >
+      <div
+        className={clsx(
+          "font-bold text-xl text-white rounded-full w-8 h-8 flex items-center justify-center",
+          getColor(place)
+        )}
+      >
+        {place}
+      </div>
+      <div className="flex flex-col text-lg">
+        <span>{athlete.name}</span>
+        <p className="text-gray-500 text-md self-end">
+          <span>
+            {formatToHours(statistics.totalTime)},{" "}
+            {formatToKilometers(statistics.totalDistance)}
+          </span>
+        </p>
+      </div>
+    </a>
+  );
+};
+
+const getColor = (index: number) => {
+  switch (index) {
+    case 1:
+      return "bg-yellow-500";
+    case 2:
+      return "bg-zinc-500";
+    case 3:
+      return "bg-amber-700";
+    default:
+      return "bg-gray-200";
+  }
+};
+
+type AthleteListItemProps = {
+  place: number;
+  athlete: Athlete;
+  statistics: {
+    totalTime: number;
+    totalDistance: number;
+  };
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
-import clsx from "clsx";
 import Image from "next/image";
 import getAthletesWithStatistics from "./lib/getAthletesWithStatistics";
-import formatToHours from "./utils/formatToHours";
-import formatToKilometers from "./utils/formatToKilometers";
 import config from "./utils/config";
+import { AthleteListItem } from "./components/AthleteListItem";
 
 // TODO: P0 Data validation screen
 // TODO: P0 Adjust start and end date
-// TODO: P1 Link to Athlete
+// TODO: P1 Oauth flow
 // TODO: P1 Club link
 // TODO: P2 Unify imports
-// TODO: P2 Oauth flow
 // TODO: P3 Logging wrapper (start, end, errors)
 // TODO: P3 Duration progress bar
 // TODO: P3 Cumulative values with examples
@@ -41,30 +38,14 @@ const Home = async () => {
             </p>
           </div>
         </div>
-        <div className="flex flex-col gap-8 mb-8">
-          {sorted.map((athlete, index) => (
-            <div
-              key={athlete.athlete.userId}
-              className="flex gap-3 items-center"
-            >
-              <div
-                className={clsx(
-                  "font-bold text-xl text-white rounded-full w-8 h-8 flex items-center justify-center",
-                  getColor(index)
-                )}
-              >
-                {index + 1}
-              </div>
-              <div className="flex flex-col text-lg">
-                <span>{athlete.athlete.name}</span>
-                <p className="text-gray-500 text-md self-end">
-                  <span>
-                    {formatToHours(athlete.statistics.totalTime)},{" "}
-                    {formatToKilometers(athlete.statistics.totalDistance)}
-                  </span>
-                </p>
-              </div>
-            </div>
+        <div className="flex flex-col gap-4 mb-8">
+          {sorted.map((data, index) => (
+            <AthleteListItem
+              key={data.athlete.userId}
+              athlete={data.athlete}
+              place={index + 1}
+              statistics={data.statistics}
+            />
           ))}
         </div>
         <p className="text-gray-300 text-sm">
@@ -82,19 +63,6 @@ const Home = async () => {
       </footer>
     </div>
   );
-};
-
-const getColor = (index: number) => {
-  switch (index) {
-    case 0:
-      return "bg-yellow-500";
-    case 1:
-      return "bg-zinc-500";
-    case 2:
-      return "bg-amber-700";
-    default:
-      return "bg-gray-200";
-  }
 };
 
 export default Home;
