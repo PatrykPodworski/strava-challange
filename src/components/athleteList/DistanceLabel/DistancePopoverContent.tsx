b99eaa3b64a81b97550d36a949bb73c74955e7bb
Patryk Podworski <patryk@xtramile.no>
2024-09-24 21:29:33 +0200

    conditional popover data

--- src/components/athleteList/DistanceLabel/DistancePopoverContent.tsx
+++ src/components/athleteList/DistanceLabel/DistancePopoverContent.tsx
@@ -1,6 +1,8 @@
 import formatToKilometers from "@/utils/formatToKilometers";
 
+// TODO: P1 Do not show if all distances are 0
 // TODO: P2 Add icons
+// TODO: P3 Sort by distance
 export const DistancePopoverContent = ({
   walk,
   ride,
@@ -8,9 +10,9 @@ export const DistancePopoverContent = ({
 }: DistancePopoverContentProps) => {
   return (
     <div className="text-sm text-gray-700">
-      <p>Walk: {formatToKilometers(walk)} km</p>
-      <p>Run: {formatToKilometers(run)} km</p>
-      <p>Ride: {formatToKilometers(ride)} km</p>
+      {walk > 0 && <p>Walk: {formatToKilometers(walk)} km</p>}
+      {run > 0 && <p>Run: {formatToKilometers(run)} km</p>}
+      {ride > 0 && <p>Ride: {formatToKilometers(ride)} km</p>}
     </div>
   );
 };
