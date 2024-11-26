b4f7edb53a04ca4aab6b1607a01af150acd2e1e5
Patryk Podworski <patryk@xtramile.no>
2024-11-26 20:45:01 +0100

    Encode the buffer in base64 to fix build

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -3,9 +3,11 @@ import { ImageResponse } from "next/og";
 import { join } from "node:path";
 import { readFile } from "node:fs/promises";
 
+// TODO: P0 Add text to the image
 export default async function Image() {
   const logoData = await readFile(join(process.cwd(), "public/trophy.png"));
-  const logoSrc = Uint8Array.from(logoData).buffer;
+  const logoBase64 = Buffer.from(logoData).toString("base64");
+  const logoSrc = `data:image/png;base64,${logoBase64}`;
 
   return new ImageResponse(
     (
