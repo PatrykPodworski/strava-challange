55102fcbe333824f7b23a83b575ae457a9c8a280
Patryk Podworski <patryk@xtramile.no>
2024-12-18 18:04:50 +0100

    pass ref as prop

--- src/components/athleteList/AthleteList.tsx
+++ src/components/athleteList/AthleteList.tsx
@@ -1,28 +1,24 @@
-import { forwardRef } from "react";
+import { Ref } from "react";
 import { AthleteListItem } from "./AthleteListItem";
 import { ListItem } from "./ListItem";
 
-const AthleteList = forwardRef<HTMLOListElement, AthleteListProps>(
-  ({ athletes }, ref) => {
-    return (
-      <ol className="flex flex-col sm:gap-4" ref={ref}>
-        {athletes.map(({ athlete, statistics, streaks }, index) => (
-          <ListItem
-            key={athlete.userId}
-            athlete={athlete}
-            place={index + 1}
-            statistics={statistics}
-            streaks={streaks}
-          />
-        ))}
-      </ol>
-    );
-  }
-);
-
-AthleteList.displayName = "AthleteList";
-export { AthleteList };
+export const AthleteList = ({ athletes, ref }: AthleteListProps) => {
+  return (
+    <ol className="flex flex-col sm:gap-4" ref={ref}>
+      {athletes.map(({ athlete, statistics, streaks }, index) => (
+        <ListItem
+          key={athlete.userId}
+          athlete={athlete}
+          place={index + 1}
+          statistics={statistics}
+          streaks={streaks}
+        />
+      ))}
+    </ol>
+  );
+};
 
 type AthleteListProps = {
   athletes: AthleteListItem[];
+  ref?: Ref<HTMLOListElement>;
 };
