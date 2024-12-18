89acf1c64ea3f1525002fb0755d06e6c016bbed3
Patryk Podworski <patryk@xtramile.no>
2024-12-18 18:46:16 +0100

    delete moved code

--- src/app/kkdg/page.tsx
+++ src/app/kkdg/page.tsx
@@ -2,13 +2,7 @@ import { SingleStatList } from "@/components/athleteList/AthleteList";
 import { ChallengeProgress } from "@/components/ChallengeProgress";
 import { getTodayChallengeDay } from "@/lib/challengeProgress/getTodayChallengeDay";
 import { data } from "./data";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { SourcePopover } from "./SourcePopover";
 
 const Home = async () => {
   const currentDay = getTodayChallengeDay();
@@ -21,16 +15,5 @@ const Home = async () => {
     </>
   );
 };
-
-export const SourcePopover = () => (
-  <Popover>
-    <PopoverTrigger>
-      <Button variant="ghost">Source</Button>
-    </PopoverTrigger>
-    <PopoverContent>
-      <Image src="/2024-kkdg.png" alt="KKDG 2024" width={588} height={749} />
-    </PopoverContent>
-  </Popover>
-);
 
 export default Home;
