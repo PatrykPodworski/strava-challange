2ed7e7dab68ce19b5d39c5c955410256da99b93e
Patryk Podworski <patryk.podworski@gmail.com>
2025-01-01 19:36:08 +0100

    Validate year when calculating the challenge day

--- src/lib/challengeProgress/getTodayChallengeDay.ts
+++ src/lib/challengeProgress/getTodayChallengeDay.ts
@@ -4,13 +4,48 @@ import { getDayOfYear } from "date-fns";
 const NEXT_PUBLIC_CHALLENGE_START_DATE =
   process.env.NEXT_PUBLIC_CHALLENGE_START_DATE;
 
+const NEXT_PUBLIC_CHALLENGE_END_DATE =
+  process.env.NEXT_PUBLIC_CHALLENGE_END_DATE;
+
+// TODO: P2 Support multiple years
 export const getTodayChallengeDay = (today?: UTCDate) => {
   if (!NEXT_PUBLIC_CHALLENGE_START_DATE) {
     throw new Error("NEXT_PUBLIC_CHALLENGE_START_DATE is not defined");
   }
+  if (!NEXT_PUBLIC_CHALLENGE_END_DATE) {
+    throw new Error("NEXT_PUBLIC_CHALLENGE_END_DATE is not defined");
+  }
 
-  const todayDay = getDayOfYear(today ?? new UTCDate());
-  const startDay = getDayOfYear(new UTCDate(NEXT_PUBLIC_CHALLENGE_START_DATE));
+  const { day: startDay, year: startYear } = getData(
+    new UTCDate(NEXT_PUBLIC_CHALLENGE_START_DATE)
+  );
+  const { day: endDay, year: endYear } = getData(
+    new UTCDate(NEXT_PUBLIC_CHALLENGE_END_DATE)
+  );
+  const { day: todayDay, year: todayYear } = getData(today ?? new UTCDate());
+
+  if (todayYear > endYear) {
+    return endDay;
+  }
+
+  if (todayYear < startYear) {
+    return 0;
+  }
+
+  if (todayDay > endDay) {
+    return endDay;
+  }
+
+  if (todayDay < startDay) {
+    return 0;
+  }
+
   const currentDay = todayDay - startDay + 1;
   return currentDay;
+};
+
+const getData = (date: UTCDate) => {
+  const year = date.getFullYear();
+  const day = getDayOfYear(date);
+  return { year, day };
 };
